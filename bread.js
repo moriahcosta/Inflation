273a44cb49b949e6ef1232bc1d79649be65e565d
Moriah Costa <moriahcosta@gmail.com>
2017-04-18 05:21:53 -0700

    Add files via upload

---
+++ bread.js
@@ -0,0 +1,78 @@
+var bread=[
+  {
+    "Year": 1998,
+    "Pence": 51
+  },
+  {
+    "Year": 1999,
+    "Pence": 51
+  },
+  {
+    "Year": 2000,
+    "Pence": 52
+  },
+  {
+    "Year": 2001,
+    "Pence": 51
+  },
+  {
+    "Year": 2002,
+    "Pence": 57
+  },
+  {
+    "Year": 2003,
+    "Pence": 58
+  },
+  {
+    "Year": 2004,
+    "Pence": 64
+  },
+  {
+    "Year": 2005,
+    "Pence": 71
+  },
+  {
+    "Year": 2006,
+    "Pence": 81
+  },
+  {
+    "Year": 2007,
+    "Pence": 90
+  },
+  {
+    "Year": 2008,
+    "Pence": 119
+  },
+  {
+    "Year": 2009,
+    "Pence": 123
+  },
+  {
+    "Year": 2010,
+    "Pence": 120
+  },
+  {
+    "Year": 2011,
+    "Pence": 120
+  },
+  {
+    "Year": 2012,
+    "Pence": 124
+  },
+  {
+    "Year": 2013,
+    "Pence": 130
+  },
+  {
+    "Year": 2014,
+    "Pence": 116
+  },
+  {
+    "Year": 2015,
+    "Pence": 104
+  },
+  {
+    "Year": 2016,
+    "Pence": 100
+  }
+]
